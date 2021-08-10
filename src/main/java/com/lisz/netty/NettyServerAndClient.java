ecbc27bd277f9e68eb953900f8a92f37bec38d94
lisz1012 <lisz1012@163.com>
2021-08-09 21:56:04 -0700

    单独使用一下Netty，写Server和Client

---
+++ src/main/java/com/lisz/netty/NettyServerAndClient.java
@@ -0,0 +1,68 @@
+package com.lisz.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+/**
+ * Netty Server: start and connect this machine from: another host with:
+ *    `nc 192.168.1.102 9090`
+ * Netty Client: start nc with:
+ *    `nc -l 192.168.1.99 9090`
+ *  and connect to 192.168.1.99 with the program,and send message from server
+ */
+public class NettyServerAndClient {
+
+	@Test
+	public void nettyServer() throws Exception {
+		NioEventLoopGroup group = new NioEventLoopGroup(1);
+		ServerBootstrap serverBootstrap = new ServerBootstrap();
+		ChannelFuture bind = serverBootstrap.group(group, group)
+				.channel(NioServerSocketChannel.class)
+				.childHandler(new ChannelInitializer<NioSocketChannel>() {
+					@Override
+					protected void initChannel(NioSocketChannel ch) throws Exception {
+						ch.pipeline().addLast(new MyInHandler());
+					}
+				})
+				.bind("192.168.1.102", 9090);
+		bind.sync().channel().closeFuture().sync();
+	}
+
+
+
+	@Test
+	public void nettyClient() throws Exception {
+		NioEventLoopGroup group = new NioEventLoopGroup(1);
+		Bootstrap bootstrap = new Bootstrap();
+		ChannelFuture connect = bootstrap
+				.group(group)
+				.channel(NioSocketChannel.class)
+				.handler(new ChannelInitializer<SocketChannel>() {
+					@Override
+					protected void initChannel(SocketChannel ch) throws Exception {
+						ch.pipeline().addLast(new MyInHandler());
+					}
+				})
+				.connect(new InetSocketAddress("192.168.1.99", 9090));
+		Channel client = connect.sync().channel();
+
+		ByteBuf buf = Unpooled.copiedBuffer("Hello server.".getBytes());
+		ChannelFuture send = client.writeAndFlush(buf);
+		send.sync();
+
+		client.closeFuture().sync();
+	}
+}
