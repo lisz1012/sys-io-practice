845ba00f315d8ef9d5fedabf076de8fff004f82f
lisz1012 <lisz1012@163.com>
2021-08-18 00:39:47 -0700

    Add notes

--- src/main/java/com/lisz/netty/rpc/MyRPCTest.java
+++ src/main/java/com/lisz/netty/rpc/MyRPCTest.java
@@ -136,7 +136,7 @@ public class MyRPCTest {
 
 	private static MyHeader createHeader(byte[] msgBody) {
 		final MyHeader myHeader = new MyHeader();
-		int size = msgBody.length;
+		int size = msgBody.length; // 　java序列化为啥长度不一样，因为类的全限定名可能跟周老师的有些许不同
 		int f = 0x14141414;
 		long requestId = Math.abs(UUID.randomUUID().getLeastSignificantBits());
 		myHeader.setFlag(f);
