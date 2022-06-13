b9c5a5687a85a7a52828c21bd763aba9fcf69139
lisz1012 <lisz1012@163.com>
2022-06-12 17:07:23 -0700

    Test for volatile

---
+++ src/test/java/com/lisz/VolatileTest.java
@@ -0,0 +1,42 @@
+package com.lisz;
+
+public class VolatileTest {
+    private static volatile int COUNTER = 0;
+
+    public static void main(String[] args) {
+        new ChangeListener().start();
+        new ChangeMaker().start();
+    }
+
+    static class ChangeListener extends Thread {
+        @Override
+        public void run() {
+            int threadValue = COUNTER;
+            while ( threadValue < 5){
+                if( threadValue!= COUNTER){
+                    System.out.println("Got Change for COUNTER : " + COUNTER + "");
+                    threadValue= COUNTER;
+                }
+//                try {
+//                    Thread.sleep(0); // Thread.sleep(0);也会让输出结果在没有volatile的情况下变正确
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+            }
+        }
+    }
+
+    static class ChangeMaker extends Thread{
+        @Override
+        public void run() {
+            int threadValue = COUNTER;
+            while (COUNTER <5){
+                System.out.println("Incrementing COUNTER to : " + (threadValue+1) + "");
+                COUNTER = ++threadValue;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+        }
+    }
+}
