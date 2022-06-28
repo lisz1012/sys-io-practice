157428375df14beae5d8fb8e6bff0d88ddb6db0a
lisz1012 <lisz1012@163.com>
2022-06-28 00:02:31 -0700

    Add comments

--- src/main/java/com/lisz/netty/NettyServerAndClient.java
+++ src/main/java/com/lisz/netty/NettyServerAndClient.java
@@ -27,7 +27,7 @@ public class NettyServerAndClient {
 
 	@Test
 	public void nettyServer() throws Exception {
-		// IO 线程
+		// IO 线程, 这里没有设置boss NioEventloopGroup
 		NioEventLoopGroup group = new NioEventLoopGroup(1);
 		ServerBootstrap serverBootstrap = new ServerBootstrap();
 		ChannelFuture bind = serverBootstrap.group(group, group)
